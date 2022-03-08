2f837bc7c97344fb2679dad7f4b4c45227fe2839
mmaxgon <mmaxgon@rambler.ru>
2022-03-08 03:15:08 +0300

    scipy-BB-start

--- maxgon-MINLP-BB.py
+++ maxgon-MINLP-BB.py
@@ -2,164 +2,66 @@
 # MINLP with scipy and branch and bound
 ##############################################################################
 
-'''
-var x{j in 1..3} >= 0;
-x[2], x[3] integer
-
-# Objective function to be minimized.
-minimize obj:
-        1000 - x[1]^2 - 2*x[2]^2 - x[3]^2 - x[1]*x[2] - x[1]*x[3];
-
-# Equality constraint.
-s.t. c1: 8*x[1] + 14*x[2] + 7*x[3] - 56 = 0;
-
-# Inequality constraint.
-s.t. c2: x[1]^2 + x[2]^2 + x[3]^2 -25 >= 0;
-'''
-
+from collections import namedtuple
 import numpy as np
-import scipy as sp
+import scipy.optimize as sp
 
-from scipy import optimize
-from scipy.optimize import minimize
-from scipy.optimize import Bounds
-from scipy.optimize import LinearConstraint
-from scipy.optimize import NonlinearConstraint
+##############################################################################
+# Объекты описания задачи
+##############################################################################
 
-def rosen(x):
-#	The Rosenbrock function
-	return sum(100.0*(x[1:]-x[:-1]**2.0)**2.0 + (1-x[:-1])**2.0)
+# Переменные решения
+dvars = namedtuple("dvars", ["n", "ix_int", "ix_cont", "x0"])
+# Границы
+bounds = namedtuple("bounds", ["lb", "ub"])
+# Цель
+objective = namedtuple("objective", ["n", "fun"])
+# Линейные ограничения
+linear_constraints = namedtuple("linear_constraints", ["m", "A", "bounds"])
+# Нелинейные ограничения
+nonlinear_constraints = namedtuple("nonlinear_constraints", ["m", "fun", "bounds"])
+# Описание оптимизационной задачи
+optimization_problem = namedtuple("optimization_problem", ["dvars", "bounds", "objective", "linear_constraints", "nonlinear_constraints"])
 
-x0 = np.array([0.5, 0])
+##############################################################################
+# Абстрактное описание задачи
+##############################################################################
 
-bounds = Bounds([0, -0.5], [1.0, 2.0])
-# 0 <= x0 <= 1, -0.5 <= x1 <= 2.0
+# Начальное значение
+x0 = [2]*3
+# Переменные решения
+decision_vars = dvars(3, [1, 2], [0], x0)
+# Границы
+var_bounds = bounds([0, 0, 0], [10, 10, 10])
 
-linear_constraint = LinearConstraint([[1, 2], [2, 1]], [-np.inf, 1], [1, 1])
-# x0 + 2*x1 <= 1
-# 1 <= 2*x0 + x1 <= 1
+# Целевая функция
+def obj(x):
+	return -(1000 - x[0]**2 - 2*x[1]**2 - x[2]**2 - x[0]*x[1] - x[0]*x[2])
+objective_fun = objective(3, obj)
 
-def cons_f(x):
-	return [x[0]**2 + x[1], x[0]**2 - x[1]]
-
-nonlinear_constraint = NonlinearConstraint(cons_f, -np.inf, 1)
-# x0**2 + x1 <= 1
-# x0**2 - x1 <= 1
-
-rosen(x0)
-x0 <= bounds.ub
-x0 >= bounds.lb
-
-np.dot(linear_constraint.A, x0) <= linear_constraint.ub 
-np.dot(linear_constraint.A, x0) >= linear_constraint.lb 
-
-np.array(nonlinear_constraint.fun(x0)) <= nonlinear_constraint.ub
-np.array(nonlinear_constraint.fun(x0)) >= nonlinear_constraint.lb
-
-#############################################################################
-# SLSQP, trust-constr
-#############################################################################
-# Local optimization
-methods = [
-	#,'Nelder-Mead' 
-	#,'Powell' 
-	#,'CG'
-	#,'BFGS'
-	#'Newton-CG', 
-	#,'L-BFGS-B' 
-	#,'TNC'
-	#'COBYLA', 
-	'SLSQP'
-	,'trust-constr' 
-	#'dogleg', 
-	#'trust-ncg', 
-	#'trust-exact', 
-	#'trust-krylov'
-]
-results = dict()
-for method in methods:
-	res = minimize(
-		fun = rosen, 
-		bounds = bounds,
-		constraints = [linear_constraint, nonlinear_constraint],
-		x0 = x0, 
-		method = method, 
-		options = {'verbose': 0}
+# Нелинейные ограничения
+def non_lin_cons_fun(x):
+	return [
+			x[0]**2 + x[1]**2 + x[2]**2 - 25,
+			x[1]**2 + x[2]**2 - 12
+		]
+non_lin_cons = nonlinear_constraints(
+		2,
+		non_lin_cons_fun,
+		bounds([-np.Inf, -np.Inf], [0, 0])
 	)
-	results[method] = res
-	#print(res.x)
-	print(method + ": " + str(res.fun))
 
-#############################################################################
-# COBYLA
-#############################################################################
+# Линейные ограничения
+lin_cons = linear_constraints(
+		1,
+		[
+			[8, 14, 7]
+		],
+		bounds([56], [56])
+	)
 
-def cons_lin(x):
-	return -np.concatenate((
-				np.dot(linear_constraint.A, x) - linear_constraint.ub,
-				linear_constraint.lb - np.dot(linear_constraint.A, x)
-			))
-cons_lin(x0)
+opt_prob = optimization_problem(decision_vars, var_bounds, objective_fun, lin_cons, non_lin_cons)
 
-def cons_nonlin(x):
-	return -(np.array([x[0]**2 + x[1], x[0]**2 - x[1]]) - nonlinear_constraint.ub)
-cons_nonlin(x0)
-
-cons = (
-	{'type': 'ineq', 'fun': cons_nonlin}
-	,{'type': 'ineq', 'fun': cons_lin}
-)
-results["COBYLA"] = minimize(
-	fun = rosen, 
-	constraints = cons,
-	x0 = x0, 
-	method = "COBYLA", 
-	options = {'verbose': 0}
-)
-
-rosen(res.x)
-
-# Global optimization
-#############################################################################
-# SHGO
-#############################################################################
-
-x = results["trust-constr"].x	
-
-bounds = [(0, 1), (-0.5, 2.0)]
-
-def cons_lin1(x):
-	return cons_lin(x)[0]
-def cons_lin2(x):
-	return cons_lin(x)[1]
-def cons_lin3(x):
-	return cons_lin(x)[2]
-def cons_lin4(x):
-	return cons_lin(x)[3]
-
-def cons_nonlin1(x):
-	return cons_nonlin(x)[0]
-def cons_nonlin2(x):
-	return cons_nonlin(x)[1]
-
-cons = (
-	{'type': 'ineq', 'fun': cons_nonlin1}
-	,{'type': 'ineq', 'fun': cons_nonlin2}
-	,{'type': 'ineq', 'fun': cons_lin1}
-	,{'type': 'ineq', 'fun': cons_lin2}
-	#,{'type': 'ineq', 'fun': cons_lin3}
-	#,{'type': 'ineq', 'fun': cons_lin4}
-	#,{'type': 'eq', 'fun': h1}
-)
-	
-results["shgo"] = optimize.shgo(
-	func = rosen,
-	bounds = bounds,
-	constraints = cons,
-	sampling_method = 'sobol',
-	minimizer_kwargs = {"method": "SLSQP"} 
-)		 
-
-for key in results:
-	print(key + ":" + str(results[key].fun))
-	
+##############################################################################
+# Решение
+##############################################################################
