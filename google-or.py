1a277829894c2d1ff2caa3f5569c723e4caf2d4c
Maxim Goncharov <mmaxgon@rambler.ru>
2022-03-28 14:14:02 +0300

    ortools.linear_solver

--- google-or.py
+++ google-or.py
@@ -2,6 +2,43 @@
 https://developers.google.com/optimization/reference/python/sat/python/cp_model
 https://github.com/google/or-tools/blob/stable/ortools/sat/samples/
 """
+
+###########################################################################
+# MILP
+###########################################################################
+from ortools.linear_solver import pywraplp
+
+solver = pywraplp.Solver.CreateSolver('SCIP')
+solver = pywraplp.Solver.CreateSolver('GLOP')
+solver = pywraplp.Solver.CreateSolver('CBC')
+solver = pywraplp.Solver.CreateSolver('CLP')
+
+# x and y are integer non-negative variables.
+infinity = solver.infinity()
+x = solver.IntVar(0.0, infinity, 'x')
+y = solver.IntVar(0.0, infinity, 'y')
+print('Number of variables =', solver.NumVariables())
+
+# x + 7 * y <= 17.5.
+solver.Add(x + 7 * y <= 17.5)
+# x <= 3.5.
+solver.Add(x <= 3.5)
+print('Number of constraints =', solver.NumConstraints())
+
+# Maximize x + 10 * y.
+solver.Maximize(x + 10 * y)
+
+status = solver.Solve()
+if status == pywraplp.Solver.OPTIMAL:
+    print('Solution:')
+    print('Objective value =', solver.Objective().Value())
+    print('x =', x.solution_value())
+    print('y =', y.solution_value())
+else:
+    print('The problem does not have an optimal solution.')
+###########################################################################
+# CP
+###########################################################################
 from ortools.sat.python import cp_model
 
 model = cp_model.CpModel()
