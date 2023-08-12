9140fe27174d35bc88d9a1e8f735808904f59e7c
mmaxgon <mmaxgon@rambler.ru>
2023-08-12 09:34:07 +0300

    scipy cutting stock

--- cutting_stock.py
+++ cutting_stock.py
@@ -60,3 +60,61 @@ for i in range(100):
 
 # Получили паттерны - теперь решаем целочисленную задачу
 sol = solve_master_problem(integrality=1)
+
+###################################################################################################################
+# SCIP - один шаг
+###################################################################################################################
+import numpy as np
+from pyscipopt import Model as scip_Model
+
+# Длина доски
+L = 10
+# Длины брусков
+l_lens = [1, 3, 5]
+# Потребности в брусках
+l_demands = np.array([19, 9, 7])
+# Число разных брусков
+l_count = len(l_lens)
+
+# Первоначальный набор шаблонов нарезки (тупой: шаблон состоит из одного бруска)
+patterns = np.eye(l_count)
+
+scip_model = scip_Model("SCIP cutting stock")
+
+for iter in range(10):
+	scip_model.freeTransform()
+	m = patterns.shape[1]
+
+	x = [scip_model.addVar(name="x{0}".format(i), vtype="I", lb=0, ub=20) for i in range(m)]
+	x_new = scip_model.addVar(name="x_new", vtype="I", lb=0, ub=20)
+
+	y = [scip_model.addVar(name="y{0}".format(i), vtype="I", lb=0, ub=10) for i in range(l_count)]
+	knapsack_constr = scip_model.addCons(
+		cons=sum(y[i] * l_lens[i] for i in range(l_count)) <= L,
+		name=f"knapsack_constr"
+	)
+
+	demand_constr = [scip_model.addCons(
+		cons=sum(x[j] * patterns[i, j] for j in range(m)) + y[i] * x_new >= l_demands[i],
+		name=f"demand_constr_{i}"
+	) for i in range(l_count)]
+
+	scip_model.setObjective(sum(x[j] for j in range(m)) + x_new, sense='minimize')
+
+	scip_model.optimize()
+	sol = scip_model.getBestSol()
+
+	print(scip_model.getStatus())
+	print([(x, sol[x]) for x in scip_model.getVars()])
+	if sol[x_new] <= 0:
+		print("done!")
+		break
+	else:
+		for i in range(m):
+			print(f"Шаблон: {str(patterns[:, i])} используем {sol[x[i]]} раз")
+		print(f"Шаблон: {str([sol[y[i]] for i in range(l_count)])} используем {sol[x_new]} раз")
+		patterns = np.hstack([patterns, np.array([sol[y[i]] for i in range(l_count)]).reshape(-1, 1)])
+
+for i in range(m):
+	print(f"Шаблон: {str(patterns[:, i])} используем {sol[x[i]]} раз")
+
